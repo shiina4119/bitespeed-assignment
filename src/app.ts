ff90361c420ca749b3d21b068ed8d5c27f0a80f2
Debanga Sarma <152995083+shiina4119@users.noreply.github.com>
2025-06-19 20:57:25 +0530

    Move route logic to routes.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express, { Response } from "express";
 import config from "./config.js";
+import router from "./routes.js"
 
 const app = express();
 
-app.get("/", (_, res: Response) => {
-  res.send("Hello World!");
-});
+app.use("/", router);
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
